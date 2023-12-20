bec3ea18b3748e0d4a1c82261580fd07c5692088
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-12-20 16:08:58 +0100

    implement the use of abortcontroller

--- src/App.js
+++ src/App.js
@@ -78,21 +78,27 @@ export default function App() {
   //To fetch the movies
   useEffect(
     function () {
+      //To prevent fetching multiple data at the same time,use an AbortContoller
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
           //To handle error
           if (!res.ok) throw new Error("Something wrong in fetching movie");
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -104,6 +110,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+      //cleanup function
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
