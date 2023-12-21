c5a870121fbe9a6392de9a9deedea86a3fbd02b4
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-12-21 14:00:29 +0100

    return back to original project

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // }
 root.render(
   <React.StrictMode>
-    <Currencyconverter />
+    {/* <Currencyconverter /> */}
+    <App />
   </React.StrictMode>
 );
 
